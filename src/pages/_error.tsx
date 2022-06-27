17fc8901b70631c06364402cc5fad994c609db88
dadolyner <skulj.david@gmail.com>
2022-06-27 12:06:59 +0200

    testing data fetching with nextjs

---
+++ src/pages/_error.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const ErrorPage: NextPage = () => {
+    return (
+        <>
+            <h1>404 Not found</h1>
+        </>
+    );
+};
+
+export default ErrorPage;
--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -1,13 +1,53 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
-const EventId: NextPage = () => {
-    const router = useRouter();
-    const { id } = router.query;
+type EventsProps = {
+    id: string;
+    image: string;
+    event_name: string;
+    location: string;
+    description: string;
+    date: string;
+    max_users: number;
+    userId: string;
+};
+
+export const getServerSideProps = async ({ query }: any) => {
+    const { id } = query;
+    const response = await fetch(`http://localhost:3000/events/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+
+    return {
+        props: {
+            id: data.id,
+            image: data.image,
+            event_name: data.event_name,
+            location: data.location,
+            description: data.description,
+            date: data.date,
+            max_users: data.max_users,
+            userId: data.userId
+        }
+    };
+};
+
+const EventId: NextPage<EventsProps> = (props: EventsProps) => {
+    const { id, event_name, description, location, date, max_users } = props;
 
     return (
         <>
-            <h1>EventId {id}</h1>
+            <h1>EventId</h1>
+            
+            <div key={id}>
+                <h2>{event_name}</h2>
+                <p>{description}</p>
+                <p>{location}</p>
+                <p>{date}</p>
+                <p>{max_users}</p>
+            </div>
         </>
     );
 };
