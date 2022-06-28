a6db27180e5967920a612ccff82fa4e298c0411a
dadolyner <skulj.david@gmail.com>
2022-06-28 13:18:38 +0200

    completed typography

---
+++ src/components/Typography/typography.styled.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Header1 = styled.h1`
+    font-family: 'Poppins';
+    font-size: 4rem;
+    font-weight: 700;
+    margin: 0;
+`;
+
+export const Header2 = styled.h2`
+    font-family: 'Poppins';
+    font-size: 3rem;
+    font-weight: 500;
+    margin: 0;
+`;
+
+export const Header3 = styled.h3`
+    font-family: 'Poppins';
+    font-size: 1.5rem;
+    font-weight: 500;
+    margin: 0;
+`;
+
+export const Header4 = styled.h4`
+    font-family: 'Poppins';
+    font-size: 1.3rem;
+    font-weight: 400;
+    margin: 0;
+`;
+
+export const Body = styled.p`
+    font-family: 'Poppins';
+    font-size: 0.9rem;
+    font-weight: 400;
+    margin: 0;
+`;
+
+export const Bold = styled.p`
+    font-family: 'Poppins';
+    font-size: 0.9rem;
+    font-weight: 600;
+    margin: 0;
+`;
