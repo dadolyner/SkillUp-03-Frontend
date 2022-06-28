792400f59d2e201403fc3ed21fd6c3f9a371c5e2
dadolyner <skulj.david@gmail.com>
2022-06-28 10:28:53 +0200

    some more testing nextjs dynamic routes

--- src/pages/events/edit/[id].tsx
+++ src/pages/events/edit/[id].tsx
@@ -1,9 +1,40 @@
 import type { NextPage } from 'next';
-import { useRouter } from 'next/router';
 
-const EditEventId: NextPage = () => {
-    const router = useRouter();
-    const { id } = router.query;
+type EventsProps = {
+    id: string;
+    image: string;
+    event_name: string;
+    location: string;
+    description: string;
+    date: string;
+    max_users: number;
+    userId: string;
+};
+
+export const getServerSideProps = async ({ query }: any) => {
+    const { id } = query;
+    const response = await fetch(`http://localhost:3000/events/${id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+
+    return {
+        props: {
+            id: data.id,
+            image: data.image,
+            event_name: data.event_name,
+            location: data.location,
+            description: data.description,
+            date: data.date,
+            max_users: data.max_users,
+            userId: data.userId
+        }
+    };
+};
+
+const EditEventId: NextPage<EventsProps> = (props: EventsProps) => {
+    const { id, event_name, description, location, date, max_users } = props;
 
     return (
         <>
